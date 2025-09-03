de88d67f5d181be6c10cdbed2fdd6c82d444820d
Fanamby <hei.fanamby@gmail.com>
2025-09-03 12:05:37 +0300

    feat:amelioration du code

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+class Characteristic(BaseModel):
+    max_speed: float
+    max_fuel_capacity: float
+    
+class Car(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+    
+cars_db: List[Car] = []
+
 @app.get("/ping")
 def ping():
-    return {"message": "pong"}
+    return "pong"   
 
 @app.post("/cars", status_code=201)
-def create_car(car: dict):
+def create_car(car: Car):
+    cars_db.append(car)
     return car
 
-@app.get("/cars")
-def list_cars():
-    return {"cars": [{"car_id": 1, "make": "toyota", "model": "corolla"}]
-            [{"car_id": 2, "make": "honda", "model": "civic"}]}
-    
-@app.get("/cars/{car_id}")
-def get_car(car_id: int):
-    return{"car_id": car_id}
+@app.get("/cars", response_model=List[Car])
+def get_cars():
+    return cars_db
 
-@app.put("/cars/{car_id}")
-def update_car(car_id: int, car: dict):
-    return {"car_id": car_id, **car}
+@app.get("/cars/{id}", response_model=Car)
+def get_car(id: str):
+    for car in cars_db:
+        if car.identifier == id:
+            return car
+
+    raise HTTPException(status_code=404, detail="Car with the provided id does not exist or was not found.")
+@app.put("/cars/{id}/characteristics", response_model=Car)
+def update_car_characteristics(id: str, characteristics: Characteristic):
+    for car in cars_db:
+        if car.identifier == id:
+            car.characteristics = characteristics
+            return car
+    raise HTTPException(status_code=404, detail="Car with the provided id does not exist or was not found.")
+
 
